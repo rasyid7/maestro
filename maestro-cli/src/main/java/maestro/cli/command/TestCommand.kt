a4bce1ad2f2b7239b749df08a86be96f82001f43
Rasyid Ridho <rasyidridho7@gmail.com>
2024-08-09 12:52:04 +0700

    randomize driverHostPort

--- maestro-cli/src/main/java/maestro/cli/command/TestCommand.kt
+++ maestro-cli/src/main/java/maestro/cli/command/TestCommand.kt
@@ -212,8 +212,7 @@ class TestCommand : Callable<Int> {
 
             val results = (0 until effectiveShards).map { shardIndex ->
                 async(Dispatchers.IO) {
-                    val driverHostPort = if (!sharded) parent?.port ?: 7001 else
-                        (7001..7128).shuffled().find { port ->
+                    val driverHostPort = (7001..7128).shuffled().find { port ->
                             usedPorts.putIfAbsent(port, true) == null
                         } ?: error("No available ports found")
 
