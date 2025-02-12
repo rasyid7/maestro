6c526705d0984c10374acc194dee75446e9d06d7
Rasyid Ridho <rasyidridho7@gmail.com>
2025-02-12 08:06:13 +0700

    [feat] sleep command

--- maestro-orchestra-models/src/main/java/maestro/orchestra/MaestroCommand.kt
+++ maestro-orchestra-models/src/main/java/maestro/orchestra/MaestroCommand.kt
@@ -73,6 +73,7 @@ data class MaestroCommand(
     val setAirplaneModeCommand: SetAirplaneModeCommand? = null,
     val toggleAirplaneModeCommand: ToggleAirplaneModeCommand? = null,
     val retryCommand: RetryCommand? = null,
+    val sleepCommand: SleepCommand? = null,
 ) {
 
     constructor(command: Command) : this(
@@ -120,7 +121,8 @@ data class MaestroCommand(
         addMediaCommand = command as? AddMediaCommand,
         setAirplaneModeCommand = command as? SetAirplaneModeCommand,
         toggleAirplaneModeCommand = command as? ToggleAirplaneModeCommand,
-        retryCommand = command as? RetryCommand
+        retryCommand = command as? RetryCommand,
+        sleepCommand = command as? SleepCommand,
     )
 
     fun asCommand(): Command? = when {
@@ -169,6 +171,7 @@ data class MaestroCommand(
         setAirplaneModeCommand != null -> setAirplaneModeCommand
         toggleAirplaneModeCommand != null -> toggleAirplaneModeCommand
         retryCommand != null -> retryCommand
+        sleepCommand != null -> sleepCommand
         else -> null
     }
 
