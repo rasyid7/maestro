28473a31d2735a65b3070d10e18e3933eac208eb
Rasyid Ridho <rasyidridho7@gmail.com>
2025-02-12 08:06:13 +0700

    [feat] sleep command

--- maestro-orchestra/src/main/java/maestro/orchestra/Orchestra.kt
+++ maestro-orchestra/src/main/java/maestro/orchestra/Orchestra.kt
@@ -311,6 +311,7 @@ class Orchestra(
             is SetAirplaneModeCommand -> setAirplaneMode(command)
             is ToggleAirplaneModeCommand -> toggleAirplaneMode()
             is RetryCommand -> retryCommand(command, config)
+            is SleepCommand -> sleepCommand(command)
             else -> true
         }.also { mutating ->
             if (mutating) {
@@ -467,6 +468,11 @@ class Orchestra(
     private fun waitForAnimationToEndCommand(command: WaitForAnimationToEndCommand): Boolean {
         maestro.waitForAnimationToEnd(command.timeout)
 
+        return true
+    }
+
+    private fun sleepCommand(command: SleepCommand): Boolean {
+        maestro.sleep(command.time)
         return true
     }
 
